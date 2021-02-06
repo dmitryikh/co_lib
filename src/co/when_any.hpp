64100717d1c07edb675b7f1ef3ed6feb881dcd80
Dmitry Khominich <khdmitryi@gmail.com>
2021-02-06 17:24:04 +0200

    fixup! [+] co::when_any & co::when_all

--- src/co/when_any.hpp
+++ src/co/when_any.hpp
@@ -11,17 +11,22 @@ template <typename T1, typename T2>
 co::func<std::tuple<T1, T2>> when_any(co::func<T1>&& f1, co::func<T2>&& f2, co::stop_source& stop)
 {
     co::event finish;
-    std::tuple<T1, T2> res;
-    auto l1 = [&res, &finish] (co::func<T1>&& f1) -> co::func<void>
+    // std::tuple<T1, T2> res;
+    // need to use optional here because T1,2 could be not default constructable
+    std::optional<T1> res1;
+    std::optional<T2> res2;
+    auto l1 = [&res1, &finish] (co::func<T1>&& f1) -> co::func<void>
     {
-        std::get<0>(res) = co_await f1;
+        // std::get<0>(res) = co_await f1;
+        res1 = co_await f1;
         finish.notify();
     };
     auto th1 = co::thread(l1(std::move(f1)));
 
-    auto l2 = [&res, &finish] (co::func<T2>&& f2) -> co::func<void>
+    auto l2 = [&res2, &finish] (co::func<T2>&& f2) -> co::func<void>
     {
-        std::get<1>(res) = co_await f2;
+        // std::get<1>(res) = co_await f2;
+        res2 = co_await f2;
         finish.notify();
     };
     auto th2 = co::thread(l2(std::move(f2)));
@@ -32,7 +37,9 @@ co::func<std::tuple<T1, T2>> when_any(co::func<T1>&& f1, co::func<T2>&& f2, co::
     co_await th1.join();
     co_await th2.join();
 
-    co_return res;
+    assert(res1.has_value() && res2.has_value());
+
+    co_return { std::move(res1.value()), std::move(res2.value()) };
 }
 
 template <typename T1, typename T2>
